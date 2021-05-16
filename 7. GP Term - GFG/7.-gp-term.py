4299af56fbc5fa61a70bd8ed9921130e99451486
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-05-16 10:12:18 +0530

    Added solution - LeetHub

---
+++ 7. GP Term - GFG/7.-gp-term.py
@@ -0,0 +1,43 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+ # } Driver Code Ends
+#User function Template for python3
+
+class Solution:    
+    #Compelte his function
+    def termOfGP(self,A,B,N):
+        #Your code here
+        return A*((B/A)**(N-1))
+
+
+#{ 
+#Driver Code Starts.
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        AB=[int(x) for x in input().strip().split()]
+        A=AB[0]
+        B=AB[1]
+        
+        N=int(input())
+        ob=Solution()
+        print(math.floor(ob.termOfGP(A,B,N)))
+        
+        T-=1
+    
+    
+
+
+
+
+if __name__=="__main__":
+    main()
+#} Driver Code Ends
