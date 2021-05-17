6950153fad41b2c88ab0e6b8d12a4693b4a328bc
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-05-17 16:39:10 +0530

    Added solution - LeetHub

--- 4. Search an element in sorted and rotated array - GFG/4.-search-an-element-in-sorted-and-rotated-array.py
+++ 4. Search an element in sorted and rotated array - GFG/4.-search-an-element-in-sorted-and-rotated-array.py
@@ -1,45 +1,19 @@
 #User function Template for python3
 
 def Search(arr,n,k):
-    for i in range(n):
-        if(arr[i]==k):
-            return i
-    return -1
     # #code here
-    # l = 0
-    # h = n-1
-    # peak=-1
-    # while(l<=h):
-    #     mid = (l+h)//2
-    #     if((mid==0 or arr[mid]>=arr[mid-1]) and (mid==n-1 or arr[mid]>=arr[mid+1])):
-    #         peak =mid
-    #         break
-    #     elif(mid>0 and arr[mid]<arr[mid-1]):
-    #         h=mid-1
-    #     elif(mid<n-1 and arr[mid]<arr[mid+1]):
-    #         l=mid+1
-    # l = 0 
-    # h = peak
-    # while(l<=h):
-    #     mid = (l+h)//2
-    #     if(arr[mid]==k):
-    #         return mid
-    #     elif(arr[mid]>k):
-    #         h= mid-1
-    #     elif(arr[mid]<k):
-    #         l=mid+1
-    # l = peak+1
-    # h= n-1
-    # while(l<=h):
-    #     mid = (l+h)//2
-    #     if(arr[mid]==k):
-    #         return mid
-    #     elif(arr[mid]>k):
-    #         h= mid-1
-    #     elif(arr[mid]<k):
-    #         l=mid+1
-    # return -1
-    
+    low = 0
+    high = n-1
+    while(low<=high):
+        mid = (low+high)//2
+        l,m,h = arr[low],arr[mid],arr[high]
+        if(arr[mid]==k):
+            return mid
+        elif(l<=k<m or(l>m and not(m<k<=h)) ):
+            high=mid-1
+        else:
+            low=mid+1
+    return -1
 
 #{ 
 #  Driver Code Starts
