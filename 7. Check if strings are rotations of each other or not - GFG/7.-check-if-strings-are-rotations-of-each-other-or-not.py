d6f54c4bbf7ca814902ac792da578804d4775926
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-06-22 22:15:31 +0530

    Added solution - LeetHub

---
+++ 7. Check if strings are rotations of each other or not - GFG/7.-check-if-strings-are-rotations-of-each-other-or-not.py
@@ -0,0 +1,44 @@
+
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if two strings are rotations of each other or not.
+    def areRotations(self,s1,s2):
+        #code here
+        if(len(s1)!=len(s2)):
+            return False
+        for i in range(len(s1)):
+            if(s1[i:]+s1[:i] == s2):
+                return True
+        return False
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s1=str(input())
+        s2=str(input())
+        if(Solution().areRotations(s1,s2)):
+            print(1)
+        else:
+            print(0)
+    
+        
+# } Driver Code Ends
