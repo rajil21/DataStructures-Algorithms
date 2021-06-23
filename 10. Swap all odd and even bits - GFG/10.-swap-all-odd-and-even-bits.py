c3f8f5813a10b08a024604179fa7310aed511f70
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-06-23 22:59:53 +0530

    Added solution - LeetHub

---
+++ 10. Swap all odd and even bits - GFG/10.-swap-all-odd-and-even-bits.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:
+    
+    ##Complete this function
+    #Function to swap odd and even bits.
+    def swapBits(self,n):
+        #Your code here
+        e = n&0xAAAAAAAA
+        o = n&0x55555555
+        e>>=1
+        o<<=1
+        return (e|o)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        print(ob.swapBits(n))
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
