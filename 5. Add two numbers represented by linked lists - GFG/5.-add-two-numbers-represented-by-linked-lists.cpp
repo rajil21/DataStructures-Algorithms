76d9cbef4506533754c29448934a323c2e57f569
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-07-04 22:01:21 +0530

    Added solution - LeetHub

--- 5. Add two numbers represented by linked lists - GFG/5.-add-two-numbers-represented-by-linked-lists.cpp
+++ 5. Add two numbers represented by linked lists - GFG/5.-add-two-numbers-represented-by-linked-lists.cpp
@@ -63,67 +63,51 @@ class Solution
 {
     public:
     //Function to add two numbers represented by linked list.
-    Node* reverseList(Node* head)
-{
-Node* current = head;
-Node* prev = NULL;
-Node* next = NULL;
-
-while(current != NULL)
-{
-next = current -> next;
-current -> next = prev;
-prev = current;
-current = next;
-}
-return prev;
-}
-
+    
+    Node* reverseLL(Node* head){
+        Node* curr = head;
+        Node *prev = NULL,*next=NULL;
+        while(curr!=NULL){
+            next = curr->next;
+            curr->next= prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
 /* Adds contents of two linked lists and
 return the head node of resultant list */
-
-
-Node* addTwoLists(Node* first, Node* second)
-{
-first = reverseList(first);
-second = reverseList(second);
-
-int carry = 0, sum = 0;
-Node* start = NULL;
-Node* end = NULL;
-
-while(first != NULL || second != NULL)
-{
-int a = (first != NULL)?first -> data:0;
-int b = (second != NULL)?second -> data:0;
-
-sum = carry + (a + b);
-carry = (sum >= 10)?1:0;
-sum = sum % 10;
-
-if(start == NULL)
-{
-start = new Node(sum);
-end = start;
-}
-else
-{
-end -> next = new Node(sum);
-end = end -> next;
-}
-
-if(first != NULL)
-first = first -> next;
-
-if(second != NULL)
-second = second -> next;
-}
-if(carry > 0)
-end -> next = new Node(carry);
-
-start = reverseList(start);
-return start;
-}
+    Node* addTwoLists(Node* first, Node* second)
+    {
+        if(first==NULL) return second;
+        if(second==NULL) return first;
+        first = reverseLL(first);
+        second = reverseLL(second);
+        int sum,carry=0,f,s;
+        Node* start=NULL,*end=NULL;
+        while(first!=NULL || second!=NULL){
+            f = (first)?first->data:0;
+            s =  (second)?second->data:0;
+            sum = carry + f+s;
+            carry = (sum>=10)?1:0;
+            sum%=10;
+            if(start==NULL){
+                start = new Node(sum);
+                end = start;
+            }
+            else{
+                end->next = new Node(sum);
+                end =end->next;
+            }
+            if(first) first = first->next;
+            if(second) second = second->next;
+        }
+        if(carry>0){
+            end->next = new Node(carry);
+        }
+        start = reverseLL(start);
+        return start;
+    }
 };
 
 
