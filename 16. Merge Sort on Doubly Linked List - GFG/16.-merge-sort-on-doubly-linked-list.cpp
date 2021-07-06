176505be775b896f3160281345b4e094f15c9366
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-07-06 20:40:42 +0530

    Added solution - LeetHub

--- 16. Merge Sort on Doubly Linked List - GFG/16.-merge-sort-on-doubly-linked-list.cpp
+++ 16. Merge Sort on Doubly Linked List - GFG/16.-merge-sort-on-doubly-linked-list.cpp
@@ -27,99 +27,99 @@ struct Node
 	    next = prev = NULL;
 	}
 }; */
-Node *merge(Node *first, Node *second)
-{
-    // If first linked list is empty
-    if (!first)
-        return second;
+// Node *merge(Node *first, Node *second)
+// {
+//     // If first linked list is empty
+//     if (!first)
+//         return second;
  
-    // If second linked list is empty
-    if (!second)
-        return first;
+//     // If second linked list is empty
+//     if (!second)
+//         return first;
  
-    // Pick the smaller value
-    if (first->data < second->data)
-    {
-        first->next = merge(first->next,second);
-        first->next->prev = first;
-        first->prev = NULL;
-        return first;
-    }
-    else
-    {
-        second->next = merge(first,second->next);
-        second->next->prev = second;
-        second->prev = NULL;
-        return second;
-    }
-}
-Node *split(Node *head)
-{
-    Node *fast = head,*slow = head;
-    while (fast->next && fast->next->next)
-    {
-        fast = fast->next->next;
-        slow = slow->next;
-    }
-    Node *temp = slow->next;
-    slow->next = NULL;
-    return temp;
-}
-// Function to do merge sort
-Node *mergeSort(Node *head)
-{
-    if (!head || !head->next)
-        return head;
-    Node *second = split(head);
- 
-    // Recur for left and right halves
-    head = mergeSort(head);
-    second = mergeSort(second);
- 
-    // Merge the two sorted halves
-    return merge(head,second);
-}
-
-
-//  Node *merge(Node* first,Node* second ){
-//     if(!first) return second;
-//     if(!second) return first;
-//     if(first->data<second->data){
+//     // Pick the smaller value
+//     if (first->data < second->data)
+//     {
 //         first->next = merge(first->next,second);
 //         first->next->prev = first;
 //         first->prev = NULL;
 //         return first;
 //     }
-//     else{
+//     else
+//     {
 //         second->next = merge(first,second->next);
 //         second->next->prev = second;
 //         second->prev = NULL;
 //         return second;
 //     }
 // }
-// Node *split( Node *head){
-//     Node *slow=head,*fast = head;
-//     while(fast->next->next && fast->next){
-//         slow = slow->next;
+// Node *split(Node *head)
+// {
+//     Node *fast = head,*slow = head;
+//     while (fast->next && fast->next->next)
+//     {
 //         fast = fast->next->next;
+//         slow = slow->next;
 //     }
-//     Node *s = slow->next;
+//     Node *temp = slow->next;
 //     slow->next = NULL;
-//     return s;
+//     return temp;
 // }
-//  Node *mergesort( Node *head){
-//     if(!head || !head->next) return head;
-//     Node* second = split(head);
-//     head = mergesort(head);
-//     second = mergesort(second);
+// // Function to do merge sort
+// Node *mergeSort(Node *head)
+// {
+//     if (!head || !head->next)
+//         return head;
+//     Node *second = split(head);
+ 
+//     // Recur for left and right halves
+//     head = mergeSort(head);
+//     second = mergeSort(second);
+ 
+//     // Merge the two sorted halves
 //     return merge(head,second);
 // }
+
+
+ Node *merge(Node* first,Node* second ){
+    if(!first) return second;
+    if(!second) return first;
+    if(first->data<second->data){
+        first->next = merge(first->next,second);
+        first->next->prev = first;
+        first->prev = NULL;
+        return first;
+    }
+    else{
+        second->next = merge(first,second->next);
+        second->next->prev = second;
+        second->prev = NULL;
+        return second;
+    }
+}
+Node *split( Node *head){
+    Node *slow=head,*fast = head;
+    while(fast->next&& fast->next->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    Node *s = slow->next;
+    slow->next = NULL;
+    return s;
+}
+ Node *mergesort( Node *head){
+    if(!head || !head->next) return head;
+    Node* second = split(head);
+    head = mergesort(head);
+    second = mergesort(second);
+    return merge(head,second);
+}
 
 //Function to sort the given doubly linked list using Merge Sort.
  Node *sortDoubly( Node *head)
 {
 	// Your code here
-	return mergeSort(head);
+	return mergesort(head);
 }
 
 
