e8bff63283f05023cf1f54c8d14b4b0b3a283055
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-07-06 20:41:00 +0530

    Added solution - LeetHub

--- 16. Merge Sort on Doubly Linked List - GFG/16.-merge-sort-on-doubly-linked-list.cpp
+++ 16. Merge Sort on Doubly Linked List - GFG/16.-merge-sort-on-doubly-linked-list.cpp
@@ -27,60 +27,6 @@ struct Node
 	    next = prev = NULL;
 	}
 }; */
-// Node *merge(Node *first, Node *second)
-// {
-//     // If first linked list is empty
-//     if (!first)
-//         return second;
- 
-//     // If second linked list is empty
-//     if (!second)
-//         return first;
- 
-//     // Pick the smaller value
-//     if (first->data < second->data)
-//     {
-//         first->next = merge(first->next,second);
-//         first->next->prev = first;
-//         first->prev = NULL;
-//         return first;
-//     }
-//     else
-//     {
-//         second->next = merge(first,second->next);
-//         second->next->prev = second;
-//         second->prev = NULL;
-//         return second;
-//     }
-// }
-// Node *split(Node *head)
-// {
-//     Node *fast = head,*slow = head;
-//     while (fast->next && fast->next->next)
-//     {
-//         fast = fast->next->next;
-//         slow = slow->next;
-//     }
-//     Node *temp = slow->next;
-//     slow->next = NULL;
-//     return temp;
-// }
-// // Function to do merge sort
-// Node *mergeSort(Node *head)
-// {
-//     if (!head || !head->next)
-//         return head;
-//     Node *second = split(head);
- 
-//     // Recur for left and right halves
-//     head = mergeSort(head);
-//     second = mergeSort(second);
- 
-//     // Merge the two sorted halves
-//     return merge(head,second);
-// }
-
-
  Node *merge(Node* first,Node* second ){
     if(!first) return second;
     if(!second) return first;
