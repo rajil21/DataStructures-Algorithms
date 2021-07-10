1294a4d19e58b1bd3b523147a605f8e1e0c0d33f
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-07-10 21:26:36 +0530

    Added solution - LeetHub

--- 7. Maximum Rectangular Area in a Histogram - GFG/7.-maximum-rectangular-area-in-a-histogram.cpp
+++ 7. Maximum Rectangular Area in a Histogram - GFG/7.-maximum-rectangular-area-in-a-histogram.cpp
@@ -33,29 +33,6 @@ class Solution
         }
         return ma;
     }
-    // {
-    //     // Your code here
-    //     stack<int>s;
-    //     long long res=0;
-    //     long long curr=0;
-    //     for(int i=0;i<n;i++) { while(s.empty()==false && arr[s.top()]>=arr[i])
-    //         {
-    //             int tp=s.top();
-    //             s.pop();
-    //             curr= arr[tp]*(s.empty()?i:(i-s.top()-1));
-    //             res=max(curr,res);
-    //         }
-    //         s.push(i);
-    //     }
-    //     while(s.empty()==false)
-    //     {
-    //         int tp=s.top();
-    //         s.pop();
-    //         curr=arr[tp]*(s.empty()?n:(n-s.top()-1));
-    //         res=max(res,curr);
-    //     }
-    //     return res;
-    // }
 };
 
 
