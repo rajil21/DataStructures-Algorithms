859b6f8e0b86d43e30d06ce1739803ef19490bb4
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-07-10 21:26:21 +0530

    Added solution - LeetHub

--- 7. Maximum Rectangular Area in a Histogram - GFG/7.-maximum-rectangular-area-in-a-histogram.cpp
+++ 7. Maximum Rectangular Area in a Histogram - GFG/7.-maximum-rectangular-area-in-a-histogram.cpp
@@ -10,52 +10,52 @@ class Solution
     public:
     //Function to find largest rectangular area possible in a given histogram.
     long long getMaxArea(long long arr[], int n)
-    // {
-    //     // Your code here
-    //     stack<int> s;
-    //     long long area;
-    //     long long ma =-1;
-    //     int temp;
-    //     for(int i=0;i<n;i++){
-    //         while(!s.empty() && arr[s.top()]>=arr[i]){
-    //             temp = arr[s.top()];
-    //             s.pop();
-    //             area = temp*((!s.empty())?i-s.top()-1:i);
-    //             if(area>ma) ma = area;
-    //         }
-    //         s.push(i);
-    //     }
-    //     while(!s.empty()){
-    //         temp = arr[s.top()];
-    //         s.pop();
-    //         area = temp*((!s.empty())?n-s.top()-1:n);
-    //         if(area>ma) ma = area;
-    //     }
-    //     return ma;
-    // }
     {
         // Your code here
-        stack<int>s;
-        long long res=0;
-        long long curr=0;
-        for(int i=0;i<n;i++) { while(s.empty()==false && arr[s.top()]>=arr[i])
-            {
-                int tp=s.top();
+        stack<int> s;
+        long long area;
+        long long ma =-1;
+        int temp;
+        for(int i=0;i<n;i++){
+            while(!s.empty() && arr[s.top()]>=arr[i]){
+                temp = s.top();
                 s.pop();
-                curr= arr[tp]*(s.empty()?i:(i-s.top()-1));
-                res=max(curr,res);
+                area = arr[temp]*((!s.empty())?i-s.top()-1:i);
+                if(area>ma) ma = area;
             }
             s.push(i);
         }
-        while(s.empty()==false)
-        {
-            int tp=s.top();
+        while(!s.empty()){
+            temp = s.top();
             s.pop();
-            curr=arr[tp]*(s.empty()?n:(n-s.top()-1));
-            res=max(res,curr);
+            area = arr[temp]*((!s.empty())?n-s.top()-1:n);
+            if(area>ma) ma = area;
         }
-        return res;
+        return ma;
     }
+    // {
+    //     // Your code here
+    //     stack<int>s;
+    //     long long res=0;
+    //     long long curr=0;
+    //     for(int i=0;i<n;i++) { while(s.empty()==false && arr[s.top()]>=arr[i])
+    //         {
+    //             int tp=s.top();
+    //             s.pop();
+    //             curr= arr[tp]*(s.empty()?i:(i-s.top()-1));
+    //             res=max(curr,res);
+    //         }
+    //         s.push(i);
+    //     }
+    //     while(s.empty()==false)
+    //     {
+    //         int tp=s.top();
+    //         s.pop();
+    //         curr=arr[tp]*(s.empty()?n:(n-s.top()-1));
+    //         res=max(res,curr);
+    //     }
+    //     return res;
+    // }
 };
 
 
