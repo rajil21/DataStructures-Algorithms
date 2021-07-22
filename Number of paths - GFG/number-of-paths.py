a6eb1cb07ca3a827a7bd70996566c5f619274d3c
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-07-22 12:12:06 +0530

    Added solution - LeetHub

--- Number of paths - GFG/number-of-paths.py
+++ Number of paths - GFG/number-of-paths.py
@@ -3,12 +3,16 @@
 class Solution:
     def numberOfPaths (self, n, m):
         # print(a)
-        if(m==1 and n==1):
-            return 1
-        if(m==0 or n==0):
-            return 0
-        return Solution().numberOfPaths(m-1,n)+Solution().numberOfPaths(m,n-1)
-        # code here
+        dp = [[0 for i in range(m+1)] for j in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if(i==1):
+                    dp[i][j]=1
+                elif(j==1):
+                    dp[i][j]=1
+                else:
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        return dp[n][m]
 
 
 #{ 
