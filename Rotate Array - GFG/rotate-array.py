8c657c13ceace3886a04ebdb737aaac54d075321
Rajagopalan V <62236258+rajil21@users.noreply.github.com>
2021-08-24 11:58:53 +0530

    Added solution - LeetHub

---
+++ Rotate Array - GFG/rotate-array.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self,A,D,N):
+        #Your code here
+        def reverseArray(A,start,end):
+            while(start<end):
+                A[start],A[end]=A[end],A[start]
+                start+=1
+                end-=1
+        reverseArray(A,0,N-1)
+        reverseArray(A,0,N-D-1)
+        reverseArray(A,N-D,N-1)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+def main():
+    T=int(input())
+    
+    while(T>0):
+        nd=[int(x) for x in input().strip().split()]
+        N=nd[0]
+        D=nd[1]
+        A=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.rotateArr(A,D,N)
+        
+        for i in A:
+            print(i,end=" ")
+            
+        print()
+       
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
